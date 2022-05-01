6fac3cfd715a30d0aebf3828e4c14bd9326228ec
Aaron Ostroski <44450422+aaronostroski@users.noreply.github.com>
2022-05-01 17:09:28 -0300

    fix: import App on root

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+
+import App from "./App";
+
+import { GlobalStyle } from "./globals/globalStyle";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
 root.render(
